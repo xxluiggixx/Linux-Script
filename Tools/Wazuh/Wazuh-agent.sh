ce40fec78222ef97d222c0494acb9b41f6d92998
Luis María García Salado <lgarciasaladokuhl@outlook.com>
2024-05-07 12:13:00 -0300

    Add env for user and pass

--- Tools/Wazuh/Wazuh-agent.sh
+++ Tools/Wazuh/Wazuh-agent.sh
@@ -9,20 +9,22 @@ DEB_REPOSITORIE="https://packages.wazuh.com/4.x/apt/pool/main/w/wazuh-agent/wazu
 OS=$(hostnamectl | grep -i "operating" | awk '{print $3}')
 
 
-read -p "Wazuh Manager IP: " WazuhManager
+read -p "Wazuh Manager IP: " WazuhManager 
+read -p "New user name: " NEWUSER 
+read -ps 'Password for $NEWUSER: ' PASS
+
 if [[ $OS = "Ubuntu" || $OS = "Debian" ]]
     then
-    echo $OS
-    echo "Installing Wazuh Agent"
+    echo 'Installing Wazuh Agent for $OS'
     wget $DEB_REPOSITORIE -O wazuh-agent.deb && chmod +x wazuh-agent.deb
     WAZUH_MANAGER='$WazuhManager' dpkg -i wazuh-agent.deb
     else
-    echo $OS
-    echo "Installing Wazuh Agent"
+    echo 'Installing Wazuh Agent for $OS'
     wget $RPM_REPOSITORIE -O wazuh-agent.rpm && chmod +x wazuh-agent.rpm
     WAZUH_MANAGER='$WazuhManager' rpm -i wazuh-agent.rpm
 
 fi
+
 if [[ $? -eq 0 ]]
     then
     echo "Remove agent file"
@@ -38,6 +40,7 @@ systemctl enable wazuh-agent
 systemctl start wazuh-agent.service
 
 #Create user and add wazuh group
-useradd seguridad && usermod -aG wazuh seguridad
+useradd -p $(openssl passwd -1 $PASS) $NEWUSER && usermod -aG wazuh $NEWUSER
+
 #Add user config ssh
-echo "AllowUsers seguridad" >> /etc/ssh/sshd_config
+[[ -e /etc/ssh/sshd_config ]] &&  echo 'AllowUsers $NEWUSER' >> /etc/ssh/sshd_config
